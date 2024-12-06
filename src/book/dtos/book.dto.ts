a34b51371d0e797e827118443417ad94ac6a1768
mohanadft <mohanadfteha@gmail.com>
2024-12-06 23:19:08 +0200

    Add book dto
    Adding it for more of type safety and data consistency

---
+++ src/book/dtos/book.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsNumber, IsNotEmpty, IsDateString } from 'class-validator';
+
+export default class BookDto {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  category: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  publication_date: string;
+}

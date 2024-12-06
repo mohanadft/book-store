c21725ff5f2e3015fc9948a3f5b986dd9f599362
mohanadft <mohanadfteha@gmail.com>
2024-12-06 23:22:14 +0200

    Implement searchBooks function

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { QueryDto, BookDto } from './dtos';
+import { ApiResponse } from './interfaces';
+import * as books from '../assets/books.json';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  searchBooks({ price, category, date }: QueryDto): ApiResponse<BookDto> {
+    let res = books.filter((book) => {
+      let matches = true;
+
+      if (price) {
+        matches = matches && book.price === price;
+      }
+
+      if (category) {
+        matches = matches && book.category === category;
+      }
+
+      if (date) {
+        matches = matches && book.publication_date.startsWith(date);
+      }
+
+      return matches;
+    });
+
+    return {
+      data: res,
+      message:
+        res.length > 0 ? 'Books retrieved successfully' : 'No books found',
+      status: true,
+      count: res.length,
+    };
+  }
+}
